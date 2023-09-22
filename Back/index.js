b4a82d95309fe0dd38a53fcd44d234ddde81879c
MathildeCalon <mathilde.calon@oclock.school>
2023-09-22 14:21:07 +0200

    architecture et modules

--- Back/index.js
+++ Back/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const bcrypt = require('bcrypt');
-
+const pg = require('pg');
 const router = require('./router');
 
 app.use(session({
