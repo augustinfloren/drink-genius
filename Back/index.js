16711517cd09657af6a01d1894b5aab212e211f9
MathildeCalon <mathilde.calon@oclock.school>
2023-09-22 15:46:42 +0200

    fichiers de seeding commences

--- Back/index.js
+++ Back/index.js
@@ -1,12 +1,24 @@
+// Modules
 const express = require('express');
-const app = express();
 const session = require('express-session');
-const bcrypt = require('bcrypt');
 const pg = require('pg');
 const router = require('./router');
 
+// Configuration de l'app
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", "../front/views");
+app.use(router);
+app.use(express.static("../front/public"));
+
+//Configuration des sessions
 app.use(session({
     resave: false,
     saveUninitialized: false,
-    secret: dotenv
+    secret: process.env.SECRET_SESSION
 }));
+
+//Lancement du serveur
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`)
+});
