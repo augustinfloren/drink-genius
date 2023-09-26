1371e93c8632c4cde13e767e143c18fc15e75bcb
MathildeCalon <131006415+MathildeCalon@users.noreply.github.com>
2023-09-26 15:19:57 +0200

    Update router.js

--- router.js
+++ router.js
@@ -8,19 +8,5 @@ const cw = require("./controllerWrapper");
 
 // Accueil
 router.get("/", mainController.getHomePage);
-/* 
-// Connexion
-router.post("/signup", cw(userController.signup));
-router.post("/login", cw(userController.login));
-
-// Récupère la liste des cocktails
-router.get("/cocktails", cw(cocktailsController.getCocktailsList));
-
-// Récupère un cocktail
-router.get("/cocktail", cw(cocktailsController.getCocktail));
-
-// MEMBRE
-
-router.get("/profile/:id", cw()) */
 
 module.exports = router;
