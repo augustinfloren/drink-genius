d8ddcef88230546d5b5df01f8faeae3fd639a8c3
Augustin Floren <44141779+augustinfloren@users.noreply.github.com>
2023-09-26 15:39:43 +0200

    Update router.js

--- router.js
+++ router.js
@@ -8,22 +8,5 @@ const cw = require("./controllerWrapper");
 // Accueil
 router.get("/", cw(mainController.getHomePage));
 
-
-/*// Connexion
-router.post("/signup", cw(userController.signup));
-router.post("/login", cw(userController.login));
-
-// Récupère la liste des cocktails
-router.get("/cocktails", cw(cocktailsController.getCocktailsList));
-
-// Récupère un cocktail
-router.get("/cocktail", cw(cocktailsController.getCocktail));
-
-// MEMBRE
-
-router.get("/profile/:id", cw())*/
-
-
-
 module.exports = router;
 
