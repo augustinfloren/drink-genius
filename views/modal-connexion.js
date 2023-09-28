2a1794ecb921d6301ddf16f1568e3a54ef24c7a5
Stephane-ANDRE <stephane.andre@oclock.school>
2023-09-28 21:44:44 +0200

    modal test 1

--- views/modal-connexion.js
+++
@@ -1,9 +0,0 @@
-const modalContainer = document.querySelector(".modal-container");
-const modalTriggers = document.querySelectorAll(".modal-trigger");
-
-modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal))
-
-function toggleModal(event){
-  event.preventDefault();
-  modalContainer.classList.toggle("active")
-}
