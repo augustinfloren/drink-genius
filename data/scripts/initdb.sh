4c74d944002961d8b5cc177b01c10b4a03edebd9
Augustin Floren <44141779+augustinfloren@users.noreply.github.com>
2023-10-01 09:05:48 +0200

    Ajout de PHHOST et PGPORT à initdb.sh (pour déploiement)

--- data/scripts/initdb.sh
+++ data/scripts/initdb.sh
@@ -1,5 +1,7 @@
 sudo -u postgres psql -f ./data/scripts/init_db.sql
 
+export PGHOST=localhost
+export PGPORT=5432
 export PGUSER=drink_genius
 export PGPASSWORD=drink_genius
 export PGDATABASE=drink_genius
