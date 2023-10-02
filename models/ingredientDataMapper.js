80a59c358143100025c3ef2c48676c067421ec1f
MathildeCalon <mathilde.calon@oclock.school>
2023-10-02 17:31:17 +0200

    random function not OK

--- models/ingredientDataMapper.js
+++ models/ingredientDataMapper.js
@@ -1,6 +1,17 @@
 const client = require('./dbClient');
 
 const ingredientDataMapper = {
+    async getRandomIngredients(){
+        const result = await client.query(`FROM ingredient
+        ORDER BY random()SELECT
+        name, unit,
+        floor(random() * (max_quantity - min_quantity) + min_quantity) AS quantity
+        FROM ingredient
+        ORDER BY random()
+        LIMIT (3 + random() * (6 - 3))`)
+        return result.rows
+    },
+
     async getAllIngredients(){
         const result = await client.query(`SELECT name FROM ingredient`);
         return result.rows;
