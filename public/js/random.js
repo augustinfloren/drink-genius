d52dd852a72b276e757fa9f6b985495fab60fa8d
MathildeCalon <mathilde.calon@oclock.school>
2023-10-03 17:43:21 +0200

    generated ingredients to be improved in script with new cocktail button

--- public/js/random.js
+++ public/js/random.js
@@ -1,8 +1,12 @@
-let machine = document.getElementById("generator");
-let generateButton = document.getElementById("generator-generate-btn");
-let randomIngredients = [];
 
-fetch('/random', {
+let machine = document.getElementById("generator");
+let generateButton = document.querySelector(".generator-generate-btn");
+let startMachine = machine.innerHTML;
+let randomIngredients = [];
+/* let softBox = document.getElementById("generator-virgin-option-label"); */
+
+function fetchRandomIngredients(){
+return fetch('/random', {
     method: 'GET',
     headers: {
         'Content-Type': 'application/json'
@@ -13,23 +17,37 @@ fetch('/random', {
     }
     return response.json();
 }).then(data => {
+    randomIngredients = [];
     data.forEach(ingredient => {
         randomIngredients.push(ingredient)
     });
 }).catch(error => {
     console.error('Erreur', error)
-});
+})}
+;
 
-
-generateButton.addEventListener('click', async function(event){
-    event.preventDefault();
-    machine.textContent = "";
+function updateIngredients(){
+    machine.innerHTML = "";
     randomIngredients.forEach(ingredient => {
         const randomIngredientDiv = document.createElement('div');
         randomIngredientDiv.classList.add('random-ingredient');
         randomIngredientDiv.textContent =  ingredient.name + ' : ' + ingredient.quantity + ' ' + ingredient.unit;
         machine.appendChild(randomIngredientDiv)
+    });
 
-        
-    })
-})
+    const newRandomButton = document.createElement('button');
+    newRandomButton.textContent = "Nouveau cocktail !";
+    machine.appendChild(newRandomButton);
+    
+    // PB ICI = LA PAGE PASSE DE LA HOMEPAGE A LA PAGE DU COCKTAIL ALEATOIRE EN MILLISECONDE
+    newRandomButton.addEventListener('click', function(event){
+        event.preventDefault();
+        machine.innerHTML = startMachine;
+        fetchRandomIngredients().then(updateIngredients);
+        });
+}
+
+generateButton.addEventListener('click', function(event){
+    event.preventDefault();
+        fetchRandomIngredients().then(updateIngredients);
+    });
