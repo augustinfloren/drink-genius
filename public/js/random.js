ad42b85aece3bd06d622c6864533cb64a343d2dd
MathildeCalon <mathilde.calon@oclock.school>
2023-10-04 12:24:34 +0200

    random generator works with virgin option

--- public/js/random.js
+++ public/js/random.js
@@ -3,7 +3,30 @@ const random = {
     generateRandomButton : document.querySelector(".generator-generate-btn"),
     startMachine : document.getElementById("generator").innerHTML,
     randomIngredients : [],
+    virginCheckbox : document.getElementById("generator-virgin-option-checkbox"),
     
+    // RECUPERER LES INGREDIENTS SANS ALCOOL ALEATOIRES
+    fetchRandomVirginIngredients: ()=>{
+        return fetch('/randomvirgin', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => {
+            if (!response.ok){
+                throw new Error('la requete a échoué')
+            }
+            return response.json();
+        })
+        .then(data => {
+            random.randomIngredients = data;
+            console.log(random.randomIngredients)
+        })
+        .catch(error => {
+            console.error('Erreur', error)
+        })},
+
     // RECUPERER LES INGREDIENTS ALEATOIRES
     fetchRandomIngredients: ()=>{
         return fetch('/random', {
@@ -52,9 +75,14 @@ const random = {
     // AJOUTER LE LISTENER SUR LE BOUTON DE GENERATION ALEATOIRE
     addGenerateRandomButtonEventListener: ()=>{
         let generateRandomButton = document.querySelector(".generator-generate-btn");
+        let virginCheckbox = document.getElementById("generator-virgin-option-checkbox")
         generateRandomButton.addEventListener('click', async function (event){
             event.preventDefault();
+            if(virginCheckbox.checked){
+                await random.fetchRandomVirginIngredients();
+            } else {
             await random.fetchRandomIngredients();
+            }
             random.addRandomIngredients();
             random.addNewRandomIngredientsBtn();
         })
@@ -65,7 +93,11 @@ const random = {
 // EVENEMENT DE DEPART SUR LE BOUTON DE GENERATION DE COCKTAIL ALEATOIRE
 random.generateRandomButton.addEventListener('click', async function(event){
     event.preventDefault();
+    if(random.virginCheckbox.checked){
+        await random.fetchRandomVirginIngredients();
+    } else {
     await random.fetchRandomIngredients();
+    }
     random.addRandomIngredients();
     random.addNewRandomIngredientsBtn();
     random.addGenerateRandomButtonEventListener();
