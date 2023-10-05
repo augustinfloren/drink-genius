480f7b722f7901c064cd1a34318f34e3c1794f80
MathildeCalon <mathilde.calon@oclock.school>
2023-10-05 14:11:47 +0200

    profile page, favourites and parameters started

--- public/js/profilePage.js
+++ public/js/profilePage.js
@@ -1,11 +1,12 @@
-let favouritesButton = document.getElementById("mycocktails-link");
-let mainContainer = document.getElementById("main-container-profile");
+let favouritesButton = document.getElementById("favourite-link");
+let mainContainer = document.getElementById("profile-main-container");
+let accountParameters = document.getElementById("parameters-link");
 let favouriteCocktails = [];
 
 
 // RECUPERER LES INGREDIENTS ALEATOIRES
 function fetchFavouriteCocktails (){
-    fetch('/profile/favourites', {
+    return fetch('/profile/favourites', {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
@@ -18,21 +19,25 @@ function fetchFavouriteCocktails (){
         return response.json();
     })
     .then(data => {
-        console.log(data)
         favouriteCocktails = data;
     })
     .catch(error => {
         console.error('Erreur', error)
 })};
 
-
-favouritesButton.addEventListener('click', function(){
-    fetchFavouriteCocktails();
+function showFavouriteCocktails(){
+    mainContainer.textContent = "";
+    console.log(favouriteCocktails)
     favouriteCocktails.forEach(cocktail => {
         const likedCocktail = document.createElement('div');
         likedCocktail.classList.add('favourite-cocktail');
         likedCocktail.textContent = cocktail.name;
         mainContainer.appendChild(likedCocktail);
     })
+}
+
+favouritesButton.addEventListener('click', async function(){
+    await fetchFavouriteCocktails();
+    showFavouriteCocktails();
 })
 
