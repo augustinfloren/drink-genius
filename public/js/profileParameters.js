4cb37bd695ffea0521ed8dd9e097f56dd66d70b0
Augustin Floren <augustinfloren@gmail.com>
2023-10-11 12:08:57 +0200

    Style paramètres ok, à faire : ajouter suppression compte

--- public/js/profileParameters.js
+++ public/js/profileParameters.js
@@ -1,48 +1,53 @@
-/**************** PARAMETRES  ****************/
+document.addEventListener("DOMContentLoaded", function () {
 
-// ECOUTE SUR LE BOUTON MODIFIER ET AFFICHE LE FORMULAIRE DE MODIFICATION
-const parametersModificationBtn = document.getElementById("parameters-modification-btn");
-const parametersForm = document.getElementById("parameters-form");
-const parametersList = document.getElementById("parameters-list");
+  /**************** PARAMETRES  ****************/
 
-parametersModificationBtn.addEventListener("click", function () {
-// MASQUER LES INFORMATIONS ACTUELLES ET AFFICHER LE FORMULAIRE DE MODIFICATION
-  parametersForm.style.display = "block";
-  parametersList.style.display = "none";
-});
+  // ECOUTE SUR LE BOUTON MODIFIER ET AFFICHE LE FORMULAIRE DE MODIFICATION
+  const parametersModificationBtn = document.getElementById("parameters-modification-btn");
+  const parametersForm = document.getElementById("parameters-form");
+  const parametersList = document.getElementById("parameters-list");
+  const fields = parametersForm.querySelectorAll("input");
 
-function updateUserInfo (userInfo){
-  return fetch("/profile", {
-    method: "PATCH",
-    body: JSON.stringify(Object.fromEntries(new FormData(userInfo))),
-    headers: {
-      "Content-Type": "application/json"
-    },
-  })
-    .then(response => {
-      if (!response.ok){
-        throw new Error("la requete a échoué");
-      }
-      return response.json();
-    })
-    .then(data => {
-        console.log('Données mises à jour : ', data);
-        window.location.reload();
-    })
-    .catch(error => {
-      console.error("Erreur", error);
-    });}
 
-// ECOUTE SUR LE BOUTON ENREGISTRER ET ENVOYER LES DONNEES DU FORMULAIRE
-const registerBtn = document.getElementById("parameters-save-changes-btn");
-// SOUMETTRE LE FORMULAIRE AVEC LES DONNEES MODIFIEES
-registerBtn.addEventListener("click", async function (event) {
+  // ACTIVATION BOUTON SUBMIT
+  fields.forEach(function (field) {
+    field.addEventListener("input", function () {
+      parametersModificationBtn.removeAttribute("disabled");
+    });
+  });
+
+  // SUBMIT MODIFICATIONS
+  parametersModificationBtn.addEventListener("click", async function (event) {
     event.preventDefault();
-  const updateForm = document.getElementById("parameters-form");
-  await updateUserInfo(updateForm);
-});
+    const updateForm = document.getElementById("parameters-form");
+    await updateUserInfo(updateForm);
+  });
 
-/**************** SUPPRIMER SON COMPTE ****************/
-const deleteButton = document.getElementById('delete-account-button');
-deleteButton.addEventListener('click', function(){
+  function updateUserInfo (userInfo){
+    return fetch("/profile", {
+      method: "PATCH",
+      body: JSON.stringify(Object.fromEntries(new FormData(userInfo))),
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+      .then(response => {
+        if (!response.ok){
+          throw new Error("la requete a échoué");
+        }
+        return response.json();
+      })
+      .then(data => {
+          console.log('Données mises à jour : ', data);
+          window.location.reload();
+      })
+      .catch(error => {
+        console.error("Erreur", error);
+      });}
+
+  /**************** SUPPRIMER SON COMPTE ****************/
+  const deleteButton = document.getElementById('delete-account-button');
+  deleteButton.addEventListener('click', function(){
+  });
+
 });
