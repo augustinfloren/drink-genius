ace0ac06e338caa83768a7eba0b82b38c85e0829
Augustin Floren <augustinfloren@gmail.com>
2023-10-13 17:47:36 +0200

    remplacement bouton par coeur OK

--- public/js/favouritesToggle.js
+++ public/js/favouritesToggle.js
@@ -1,90 +1,74 @@
 // AFFICHAGE DU BOUTON AJOUTER/SUPPRIMER
-let addFavouriteButton = document.getElementById('add-favourites-btn');
-let deleteFavouriteButton = document.getElementById('delete-favourite-btn');
+let favouriteButton = document.querySelector('.add-favourite-btn');
 const cocktailId = document.querySelector('.cocktail-img').getAttribute('data-info');
-const message = document.getElementById('confirmation-message');
 let favouritesDb = [];
 
 // RECUPERATION DES ID DES COCKTAILS FAVORIS PAR UTILISATEUR CONNECTE
-function getFavouritesId(){
-    return fetch("/profile/usersfavourites", {
-        method: "GET",
-        headers: {
-        "Content-Type": "application/json"
-        },
+async function getFavouritesId(){
+  return await fetch("/profile/usersfavourites", {
+    method: "GET",
+    headers: {
+    "Content-Type": "application/json"
+    },
+  })
+  .then(response => {
+    if (!response.ok) {
+        throw new Error("La requête a échoué");
+    }
+    return response.json();
     })
-        .then(response => {
-        if (!response.ok) {
-            throw new Error("La requête a échoué");
-        }
-        return response.json();
-        })
-        .then(data => {
-            favouritesDb = data;
-        })
-        .catch(error => {
-        console.error("Erreur : ", error);
-        });
+    .then(data => {
+        favouritesDb = data;
+    })
+    .catch(error => {
+    console.error("Erreur : ", error);
+  });
 }
 
-// AFFICHAGE BOUTON AJOUTER/SUPPRIMER
+// AFFICHAGE BOUTON AJOUTER
 async function favouritesToggle() {
-await getFavouritesId();
-const presence = favouritesDb.some(el => el.id === parseInt(cocktailId));
-if(presence){
-    deleteFavouriteButton.style.display = "block";
-    } else {
-        addFavouriteButton.style.display = "block";
-    }
-};
+  await getFavouritesId();
+  const presence = favouritesDb.some(el => el.id === parseInt(cocktailId));
+  if(presence){
+    favouriteButton.classList.add('fa-solid');
+    favouriteButton.removeEventListener('click', addFavourite);
+    favouriteButton.addEventListener('click', deleteFavourite);
+  } else {
+    // ECOUTE SUR LE BOUTON AJOUTER AUX FAVORIS
+    favouriteButton.addEventListener("click", addFavourite);
+  };
 
-favouritesToggle();
-
-// ECOUTE SUR LE BOUTON AJOUTER AUX FAVORIS
-document.addEventListener("DOMContentLoaded", function () {
-  const addFavouritesBtn = document.getElementById("add-favourites-btn");
-  
-  if (addFavouritesBtn) {
-    addFavouritesBtn.addEventListener("click", function () {
-      const cocktailId = this.getAttribute("data-cocktail-id");
-      
-      // ECRITURE EN BDD DU NOUVEAU COCKTAIL FAVORI
-      fetch("/profile/favourites", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ cocktailId: cocktailId })
+  function addFavourite() {
+    const cocktailId = this.getAttribute("data-cocktail-id");
+    // ECRITURE EN BDD DU NOUVEAU COCKTAIL FAVORIS
+    fetch("/profile/favourites", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ cocktailId: cocktailId })
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("La requête a échoué");
+        }
+        return response.json();
       })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error("La requête a échoué");
-          }
-          return response.json();
-        })
-        .then(data => {
-          message.textContent = "Cocktail ajouté aux favoris!";
-          message.style.display = "block";
-          setTimeout(() => {
-            message.style.display = "none";
-            deleteFavouriteButton.style.display = "block";
-            addFavouriteButton.style.display = "none";
-          }, 1500);
-             
-        })
-        .catch(error => {
-          console.error("Erreur : ", error);
-          
-          
-        });
-    });
-  }
-});
+      .then(data => {
+        favouriteButton.classList.remove('fa-regular');
+        favouriteButton.classList.add('fa-solid');
+        favouriteButton.removeEventListener('click', addFavourite);
+        favouriteButton.addEventListener('click', deleteFavourite);
+      })
+      .catch(error => {
+        console.error("Erreur : ", error);
+      });
+    }
 
-// ECOUTE SUR LE BOUTON DE SUPPRESSION DES FAVORIS
-deleteFavouriteButton.addEventListener('click', function(){
-    const cocktailId = this.getAttribute('data-info');
+  // ECOUTE SUR LE BOUTON DE SUPPRESSION DES FAVORIS
 
+  function deleteFavourite() {
+    const cocktailId = this.getAttribute('data-cocktail-id');
     // SUPPRESSION DU COCKTAIL EN BDD
     fetch('/profile/favourites', {
         method: 'DELETE',
@@ -100,15 +84,16 @@ deleteFavouriteButton.addEventListener('click', function(){
         return response.json();
     })
     .then(data => {
-        message.textContent = 'Le cocktail a bien été retiré des favoris.';
-        message.style.display = "block";
-    setTimeout(() => {
-        message.style.display = "none";
-        deleteFavouriteButton.style.display = "none";
-        addFavouriteButton.style.display = "block";
-        }, 1500);
+        console.log("Delete ok")
+        favouriteButton.classList.remove('fa-solid');
+        favouriteButton.classList.add('fa-regular');
+        favouriteButton.removeEventListener('click', deleteFavourite);
+        favouriteButton.addEventListener('click', addFavourite);
     })
     .catch(error => {
         console.error('Erreur', error)
     })
-});
+  }
+}
+
+favouritesToggle();
