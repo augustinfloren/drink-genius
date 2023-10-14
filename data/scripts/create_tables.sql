c9eb3ebcde876a66d96161f51e3eb9093908303d
MathildeCalon <mathilde.calon@oclock.school>
2023-10-14 22:45:39 +0200

    PLPGSQL function OK

--- data/scripts/create_tables.sql
+++ data/scripts/create_tables.sql
@@ -164,37 +164,39 @@ CREATE OR REPLACE FUNCTION newcocktail(
     p_name TEXT,
     p_instruction TEXT,
     p_user_id INTEGER,
-    p_ingredients JSON
-) RETURNS cocktail AS $$
+    p_ingredients json
+) RETURNS VOID AS $$
 DECLARE 
     v_cocktail_id INTEGER;
-    v_ingredient record;
+    v_ingredient json;
     ingredient_id INTEGER;
     quantity INTEGER;
 
 BEGIN
 
-INSERT INTO cocktail(name, instruction, validation, user_id) 
-VALUES (
-    p_name, 
-    p_instruction, 
-    false, 
-    p_user_id)
-RETURNING id INTO v_cocktail_id;
+    INSERT INTO cocktail(name, instruction, picture, validation, user_id) 
+    VALUES (
+        p_name, 
+        p_instruction, 
+        'cocktail.png',
+        false, 
+        p_user_id)
+    RETURNING id INTO v_cocktail_id;
 
-FOR v_ingredient IN SELECT * FROM json_array_elements(p_ingredients) LOOP
-    ingredient_id := (v_ingredient->>'ingredient_id')::INTEGER;
-    quantity := (v_ingredient->>'givenQuantity')::INTEGER;
+    FOR v_ingredient IN 
+        SELECT * FROM json_array_elements(p_ingredients) 
+    LOOP
+        ingredient_id := (v_ingredient->>'ingredient_id')::INTEGER;
+        quantity := (v_ingredient->>'quantity')::INTEGER;
 
-INSERT INTO cocktail_contain_ingredient (cocktail_id, ingredient_id, quantity)
-VALUES (
-    v_cocktail_id, 
-    ingredient_id,
-    quantity
-);
-END LOOP;
+    INSERT INTO cocktail_contain_ingredient (cocktail_id, ingredient_id, quantity)
+    VALUES (
+        v_cocktail_id, 
+        ingredient_id,
+        quantity
+    );
+    END LOOP;
 
-RETURN v_cocktail_id;
 END;
 
 $$ LANGUAGE PLPGSQL;
