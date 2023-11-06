6539cd4c9ca2bbf95324bbcd7938e407fbe8d8e1
Augustin Floren <augustinfloren@gmail.com>
2023-11-06 11:52:11 +0100

    Ajout de commentaires dans authModal.js

--- public/js/authModal.js
+++ public/js/authModal.js
@@ -1,30 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
 
   // Modale
+  const connectionNavLink = document.querySelector(".modal-trigger-connexion");
   const modalContainer = document.getElementById("auth-modal");
-  const modal = document.querySelector(".modal");
+  const modalTitle = document.getElementById("modalTitle");
+
+  // Bouton de fermeture de la modale
+  const closeButton = document.getElementById("close-modal");
+
+  // Formulaire
+  const form = document.getElementById("auth-modal-form");
+  const lastname = document.getElementById("lastname-input");
+  const firstname = document.getElementById("firstname-input");
+  const birthdate = document.getElementById("date-input");
+  const email = document.getElementById("email-input");
+  const password = document.getElementById("password-input");
+  const confirmation = document.getElementById("confirmation-input");
+  const submitButton = document.getElementById("auth-modal-submit-button");
 
   // Lien already registered
   const alreadyRegisteredLink = document.getElementById("already-registered-link");
 
-  const main = document.querySelector("main");
-  const modalTitle = document.getElementById("modalTitle");
-  const nomInput = document.getElementById("nom-input");
-  const prenomInput = document.getElementById("prenom-input");
-  const dateInput = document.getElementById("date-input");
-  const emailInput = document.getElementById("email-input");
-  const passwordInput = document.getElementById("password-input");
-  const confirmationInput = document.getElementById("confirmation-input");
-  const form = document.getElementById("auth-modal-form");
-  const btn = document.getElementById("auth-modal-btn");
-  const closeBtn = document.getElementById("close-modal");
-  const modalTriggerConnexion = document.querySelector(".modal-trigger-connexion");
-  const fields = form.querySelectorAll("input");
-
   // Message d'erreurs pour les champs du form
-  const dateSpan = document.querySelector("#date-field span");
-  const passSpan = document.querySelector("#pass-field span");
-  const confirmationSpan = document.querySelector("#confirmation-field span");
+  const dateError = document.querySelector("#date-field span");
+  const passError = document.querySelector("#pass-field span");
+  const confirmationError = document.querySelector("#confirmation-field span");
+
+  // Flags pour contrôle du form
   let dateOK = false;
   let passOK = false;
   let confirmationOK = false;
@@ -43,72 +45,87 @@ document.addEventListener("DOMContentLoaded", function () {
     fetchSignup();
   }
 
-  // Listeners infos champs formulaire
-  // Messages d'erreurs date
+
+  // LISTENERS //
+
+  // Vérification année de naissance
   const ifWrongDateListener = (event) => {
+    // Récupération de la saisie de l'utilisateur et de la date actuelle
     const inputValue = event.target.value;
     const currentYear = new Date().getFullYear();
+    // Contrôle format date
     if (parseInt(inputValue) < 1900 || inputValue.length > 4) {
-      dateSpan.style.display = "block";
-      dateSpan.innerText = "Veuillez entrer une année de naissance valide.";
+      dateError.style.display = "block";
+      dateError.innerText = "Veuillez entrer une année de naissance valide.";
       dateOK = false;
+    // Contrôle âge
     } else if (parseInt(inputValue) > currentYear - 18) {
-      dateSpan.style.display = "block";
-      dateSpan.innerText = "Vous devez avoir au moins 18 ans.";
+      dateError.style.display = "block";
+      dateError.innerText = "Vous devez avoir au moins 18 ans.";
       dateOK = false;
     } else {
-      dateSpan.style.display="none";
+      dateError.style.display="none";
       dateOK = true;
+      // Si tous les champs OK = Activation du bouton submit
       if (dateOK && passOK && confirmationOK) {
-        btn.removeAttribute("disabled");
+        submitButton.removeAttribute("disabled");
       }
     }
   }
 
+  // Vérification mot de passe
+
   // Récupération du regex
-  const passRegex = new RegExp(passwordInput.getAttribute("pattern"));
-  // Listener mot de passe
+  const passRegex = new RegExp(password.getAttribute("pattern"));
+
   const ifWrongPassListener = (event) => {
+    // Récupération de la saisie de l'utilisateur
     const inputValue = event.target.value;
+    // Contrôle format mdp
     if (!passRegex.test(inputValue)) {
-      passSpan.style.display = "block";
-      passSpan.innerText = "Doit contenir au moins 8 caractères, une majuscule, un chiffre, et un caractère spécial.";
+      passError.style.display = "block";
+      passError.innerText = "Doit contenir au moins 8 caractères, une majuscule, un chiffre, et un caractère spécial.";
       passOK = false;
     } else {
-      passSpan.style.display ="none";
+      passError.style.display ="none";
       passOK = true;
     }
-    const confirmation = confirmationInput.value
-    if (inputValue === confirmation) {
+    // Comparaison mdp et confirmation
+    const confirmationValue = confirmation.value
+    if (inputValue === confirmationValue) {
       confirmationOK = true;
-      confirmationSpan.style.display ="none";
+      confirmationError.style.display ="none";
     } else {
       confirmationOK = false;
     }
+    // Si tous les champs OK = Activation du bouton submit
     if (dateOK && passOK && confirmationOK) {
-      btn.removeAttribute("disabled");
+      submitButton.removeAttribute("disabled");
     }
   }
 
-  // listener Confirmation
+  // Vérification Confirmation
   const comparePassListener = (event) => {
-    const password = passwordInput.value
+    // Récupération de la saisie de l'utilisateur (mot de passe et confirmation)
+    const passwordValue = password.value
     const inputValue = event.target.value;
 
-    if (inputValue !== password ) {
-      confirmationSpan.style.display = "block";
-      confirmationSpan.innerText = "Les mots de passe doivent correspondrent.";
+    // Comparaison mdp et confirmation
+    if (inputValue !== passwordValue ) {
+      confirmationError.style.display = "block";
+      confirmationError.innerText = "Les mots de passe doivent correspondrent.";
       confirmationOK = false;
     } else {
-      confirmationSpan.style.display ="none";
+      confirmationError.style.display ="none";
       confirmationOK = true;
+      // Si tous les champs OK = Activation du bouton submit
         if (dateOK && passOK && confirmationOK) {
-          btn.removeAttribute("disabled");
+          submitButton.removeAttribute("disabled");
         }
     }
   }
 
-  // Arrivée sur le site après confirmation
+  // Arrivée sur le site après validation du mail
   const urlParams = new URLSearchParams(window.location.search);
   const isConfirmed = urlParams.get('confirmed');
 
@@ -117,32 +134,32 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   // Lorsque je clique sur Connexion
-  modalTriggerConnexion.addEventListener("click", toggleModalConnexion);
+  connectionNavLink.addEventListener("click", toggleModalConnexion);
 
   function toggleModalConnexion() {
     modalTitle.style.color = "var(--theme-purple)";
     modalTitle.style.fontSize = "1.5em";
     modalContainer.classList.toggle("active");
     modalTitle.innerText = "Connexion";
-    nomInput.style.display = "none";
-    prenomInput.style.display = "none";
-    dateInput.style.display = "none";
-    confirmationInput.style.display = "none";
+    lastname.style.display = "none";
+    firstname.style.display = "none";
+    birthdate.style.display = "none";
+    confirmation.style.display = "none";
     form.style.marginTop = "-20px";
     form.action = "/login";
-    btn.style.marginTop = "0px";
-    btn.innerText = "Se connecter";
-    btn.removeAttribute("disabled");
+    submitButton.style.marginTop = "0px";
+    submitButton.innerText = "Se connecter";
+    submitButton.removeAttribute("disabled");
     alreadyRegisteredLink.innerText = "Pas encore membre ? Par ici !";
     isRegistrationModal = false;
-    dateSpan.style.display = "none";
-    passSpan.style.display = "none";
-    confirmationSpan.style.display = "none";
+    dateError.style.display = "none";
+    passError.style.display = "none";
+    confirmationError.style.display = "none";
 
     // Suppresssion listener vérification champs
-    dateInput.removeEventListener("keyup", ifWrongDateListener);
-    passwordInput.removeEventListener("keyup", ifWrongPassListener);
-    confirmationInput.removeEventListener("keyup", comparePassListener);
+    birthdate.removeEventListener("keyup", ifWrongDateListener);
+    password.removeEventListener("keyup", ifWrongPassListener);
+    confirmation.removeEventListener("keyup", comparePassListener);
 
     form.removeEventListener("submit", signupListener);
 
@@ -155,32 +172,32 @@ document.addEventListener("DOMContentLoaded", function () {
     modalTitle.style.color = "var(--theme-purple)";
     modalTitle.style.fontSize = "1.5em";
     modalTitle.innerText = "Remplissez ce formulaire puis remplissez votre verre !";
-    nomInput.style.display = "block";
-    nomInput.value = "";
-    nomInput.setAttribute("required", true);
-    prenomInput.style.display = "block";
-    prenomInput.setAttribute("required", true);
-    prenomInput.value = "";
-    dateInput.style.display = "block";
-    dateInput.setAttribute("required", true);
-    dateInput.value = "";
-    confirmationInput.style.display = "block";
-    confirmationInput.setAttribute("required", true);
-    confirmationInput.value = "";
-    emailInput.value = "";
-    passwordInput.value = "";
+    lastname.style.display = "block";
+    lastname.value = "";
+    lastname.setAttribute("required", true);
+    firstname.style.display = "block";
+    firstname.setAttribute("required", true);
+    firstname.value = "";
+    birthdate.style.display = "block";
+    birthdate.setAttribute("required", true);
+    birthdate.value = "";
+    confirmation.style.display = "block";
+    confirmation.setAttribute("required", true);
+    confirmation.value = "";
+    email.value = "";
+    password.value = "";
     form.style.marginTop = "0px";
     form.action = "/signIn";
-    btn.style.marginTop = "10px";
-    btn.innerText = "S'enregistrer";
-    btn.setAttribute("disabled", "true");
+    submitButton.style.marginTop = "10px";
+    submitButton.innerText = "S'enregistrer";
+    submitButton.setAttribute("disabled", "true");
     alreadyRegisteredLink.innerText = "Déjà inscrit ? Par ici !";
     isRegistrationModal = true;
 
     // Vérification champs
-    dateInput.addEventListener("keyup", ifWrongDateListener);
-    passwordInput.addEventListener("keyup", ifWrongPassListener);
-    confirmationInput.addEventListener("keyup", comparePassListener);
+    birthdate.addEventListener("keyup", ifWrongDateListener);
+    password.addEventListener("keyup", ifWrongPassListener);
+    confirmation.addEventListener("keyup", comparePassListener);
 
     // Supression du listener login
     form.removeEventListener("submit", loginListener);
@@ -190,46 +207,46 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   // Lorsque je bascule d'inscription à connexion
-  modalTriggerConnexion.addEventListener("click", toggleModalConnexion);
+  connectionNavLink.addEventListener("click", toggleModalConnexion);
 
   function switchModalConnexion() {
     modalTitle.style.color = "var(--theme-purple)";
     modalTitle.style.fontSize = "1.5em";
     modalTitle.innerText = "Connexion";
-    nomInput.style.display = "none";
-    nomInput.removeAttribute("required");
-    nomInput.value = "";
-    prenomInput.style.display = "none";
-    prenomInput.removeAttribute("required");
-    prenomInput.value = "";
-    dateInput.style.display = "none";
-    dateInput.removeAttribute("required");
-    dateInput.value = "";
-    confirmationInput.style.display = "none";
-    confirmationInput.removeAttribute("required");
-    confirmationInput.value = "";
-    emailInput.value = "";
-    passwordInput.value = "";
+    lastname.style.display = "none";
+    lastname.removeAttribute("required");
+    lastname.value = "";
+    firstname.style.display = "none";
+    firstname.removeAttribute("required");
+    firstname.value = "";
+    birthdate.style.display = "none";
+    birthdate.removeAttribute("required");
+    birthdate.value = "";
+    confirmation.style.display = "none";
+    confirmation.removeAttribute("required");
+    confirmation.value = "";
+    email.value = "";
+    password.value = "";
     form.style.marginTop = "-20px";
     form.action = "/login";
-    btn.innerText = "Se connecter";
-    btn.removeAttribute("disabled");
+    submitButton.innerText = "Se connecter";
+    submitButton.removeAttribute("disabled");
     alreadyRegisteredLink.innerText = "Pas encore membre ? Par ici !";
     isRegistrationModal = false;
-    dateSpan.style.display = "none";
-    passSpan.style.display = "none";
-    confirmationSpan.style.display = "none";
+    dateError.style.display = "none";
+    passError.style.display = "none";
+    confirmationError.style.display = "none";
 
     // Suppresssion listener vérification champs
-    dateInput.removeEventListener("keyup", ifWrongDateListener);
-    passwordInput.removeEventListener("keyup", ifWrongPassListener);
-    confirmationInput.removeEventListener("keyup", comparePassListener);
+    birthdate.removeEventListener("keyup", ifWrongDateListener);
+    password.removeEventListener("keyup", ifWrongPassListener);
+    confirmation.removeEventListener("keyup", comparePassListener);
 
     // Suppression du listener signup
     form.removeEventListener("submit", signupListener);
 
     // Suppression du listener infos password
-    passwordInput.removeEventListener("keyup", ifWrongPassListener);
+    password.removeEventListener("keyup", ifWrongPassListener);
 
     // Ajout du listener login
     form.addEventListener("submit", loginListener);
@@ -280,6 +297,7 @@ document.addEventListener("DOMContentLoaded", function () {
       switchModalConnexion();
       modalTitle.innerText = data;
       modalTitle.style.fontSize = "1.3em";
+      modalTitle.style.color = "green";
     })
     .catch(error => {
       modalTitle.style.color = "red";
@@ -325,7 +343,7 @@ document.addEventListener("DOMContentLoaded", function () {
       alreadyRegisteredLink.style.display = "none";
       modalTitle.style.color = "var(--theme-purple)";
       modalTitle.innerText = data;
-      closeBtn.style.display = "none";
+      closeButton.style.display = "none";
       setTimeout(() => {
         window.location.href = '/';
       }, 1000);
@@ -337,7 +355,7 @@ document.addEventListener("DOMContentLoaded", function () {
       if (error.status === 400) {
         modalTitle.innerText = error.message;
       } else {
-        modalTitle.innerText = "Une erreur s'est produite de l'authentification.";
+        modalTitle.innerText = "Une erreur s'est produite lors de l'authentification.";
       }
     })
   }
