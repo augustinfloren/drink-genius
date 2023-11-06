df90fb9d7c7c7b1e8e8168a594659a5bc33dacc0
Augustin Floren <augustinfloren@gmail.com>
2023-11-06 13:32:22 +0100

    Correction id non uniques formulaire création

--- public/js/favouritesToggle.js
+++ public/js/favouritesToggle.js
@@ -21,19 +21,18 @@ async function getFavouritesId(){
         favouritesDb = data;
     })
     .catch(error => {
-    console.error("Erreur : ", error);
+      console.log(`Erreur : ${error}`)
   });
 }
 
 
 // AFFICHAGE BOUTON AJOUTER
-
 favouriteButton.forEach(button => {
-    // Vous pouvez accéder au cocktailId à partir de l'élément de bouton actuel ici
-    const cocktailId = button.getAttribute('data-cocktail-id');
-    favouritesToggle(cocktailId, button);
+  const cocktailId = button.getAttribute('data-cocktail-id');
+  favouritesToggle(cocktailId, button);
 });
-  async function favouritesToggle(cocktailId, button) {
+
+async function favouritesToggle(cocktailId, button) {
   await getFavouritesId();
   const presence = favouritesDb.some(el => el.id === parseInt(cocktailId));
   if(presence){
@@ -43,63 +42,61 @@ favouriteButton.forEach(button => {
   } else {
     // ECOUTE SUR LE BOUTON AJOUTER AUX FAVORIS
     button.addEventListener('click', addFavourite);
-  };
+};
 
-  function addFavourite() {
-    const cocktailId = this.getAttribute("data-cocktail-id");
-    // ECRITURE EN BDD DU NOUVEAU COCKTAIL FAVORIS
-    fetch("/profile/favourites", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ cocktailId: cocktailId })
+function addFavourite() {
+  const cocktailId = this.getAttribute("data-cocktail-id");
+  // ECRITURE EN BDD DU NOUVEAU COCKTAIL FAVORIS
+  fetch("/profile/favourites", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ cocktailId: cocktailId })
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("La requête a échoué");
+      }
+      return response.json();
     })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error("La requête a échoué");
-        }
-        return response.json();
-      })
-      .then(data => {
-        button.classList.remove('fa-regular');
-        button.classList.add('fa-solid');
-        button.removeEventListener('click', addFavourite);
-        button.addEventListener('click', deleteFavourite);
-      })
-      .catch(error => {
-        console.error("Erreur : ", error);
-      });
-    }
-
-    // ECOUTE SUR LE BOUTON DE SUPPRESSION DES FAVORIS
-
-  function deleteFavourite() {
-    const cocktailId = this.getAttribute('data-cocktail-id');
-    // SUPPRESSION DU COCKTAIL EN BDD
-    fetch('/profile/favourites', {
-      method: 'DELETE',
-        body: JSON.stringify({ cocktailId: cocktailId }),
-        headers: {
-          'Content-Type': 'application/json',
-        }
-      })
-      .then(response => {
-        if (!response.ok){
-          throw new Error('la requete a échoué')
-        }
-        return response.json();
-      })
-      .then(data => {
-        button.classList.remove('fa-solid');
-        button.classList.add('fa-regular');
-        button.removeEventListener('click', deleteFavourite);
-        button.addEventListener('click', addFavourite);
+    .then(data => {
+      button.classList.remove('fa-regular');
+      button.classList.add('fa-solid');
+      button.removeEventListener('click', addFavourite);
+      button.addEventListener('click', deleteFavourite);
     })
     .catch(error => {
-        console.error('Erreur', error)
-      })
-    }
+      console.log(`Erreur : ${error}`)
+    });
   }
 
-  favouritesToggle();
+  // ECOUTE SUR LE BOUTON DE SUPPRESSION DES FAVORIS
+
+function deleteFavourite() {
+  const cocktailId = this.getAttribute('data-cocktail-id');
+  // SUPPRESSION DU COCKTAIL EN BDD
+  fetch('/profile/favourites', {
+    method: 'DELETE',
+      body: JSON.stringify({ cocktailId: cocktailId }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    .then(response => {
+      if (!response.ok){
+        throw new Error('la requete a échoué')
+      }
+      return response.json();
+    })
+    .then(data => {
+      button.classList.remove('fa-solid');
+      button.classList.add('fa-regular');
+      button.removeEventListener('click', deleteFavourite);
+      button.addEventListener('click', addFavourite);
+  })
+  .catch(error => {
+      console.log(`Erreur : ${error}`)
+    })
+  }
+}
