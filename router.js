1147b9f647dd7ed004c1db48b05588db87bfa444
MathildeCalon <mathilde.calon@oclock.school>
2023-11-07 15:25:10 +0100

    no validator

--- router.js
+++ router.js
@@ -4,7 +4,6 @@ const cw = require("./services/controllerWrapper");
 const validationService = require("./services/validationService");
 const middleware404 = require("./services/middleware404");
 const auth = require("./services/authMiddleware");
-const sanitizer = require('./services/sanitizeService');
 
 // Accueil
 router.get("/", cw(mainController.renderHomePage));
@@ -30,13 +29,13 @@ router.delete("/profile", auth.isAuthed, cw(userController.deleteProfile));
 
 // User - Profil
 router.get("/profile/parameters", auth.isAuthed, cw(userController.renderProfilePage));
-router.patch("/profile", auth.isAuthed, sanitizer, cw(userController.updateProfile));
+router.patch("/profile", auth.isAuthed, cw(userController.updateProfile));
 router.get("/profile/usersfavourites", auth.isAuthed, cw(userController.getFavouriteCocktails));
 router.get("/profile/favourites", auth.isAuthed, cw(userController.renderFavouritesPages));
 router.post("/profile/favourites", auth.isAuthed, cw(userController.addToFavouritesByUser));
 router.delete("/profile/favourites", auth.isAuthed, cw(userController.deleteFavourite))
 router.get("/profile/newcocktail", auth.isAuthed, cw(userController.renderNewCocktailPage));
-router.post("/profile/newcocktail", auth.isAuthed, sanitizer, cw(userController.addNewCocktail));
+router.post("/profile/newcocktail", auth.isAuthed, cw(userController.addNewCocktail));
 router.get("/ingredients", auth.isAuthed, cw(userController.displayIngredients));
 router.get("/profile/usercocktails", auth.isAuthed, cw(userController.renderUserCocktailsPage));
 
